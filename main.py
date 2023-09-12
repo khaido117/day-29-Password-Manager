d759205c117a5ffc3e3ee234523f578840c58ad0
Khai Do <khaitroyy@gmail.com>
2023-09-12 16:52:44 -0500

    add password generator

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+#Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letter = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(letters) for _ in range(randint(2, 4))]
+
+    password_list = password_letter + password_symbols + password_numbers
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -8,8 +29,15 @@ def save_info():
     password_content = password_entry.get()
     email_content = email_entry.get()
 
-    with open("password.txt", mode= "a") as file:
-        file.writelines(website_content + "|" + email_content + "|" + password_content + "\n")
+    if len(website_content) == 0 or len(password_content) == 0:
+            messagebox.showinfo(title="Oops!", message="Please enter information to continue.")
+    else: 
+
+        is_ok = messagebox.askokcancel(title="website", message= f"Please confirm you info: \n Email: {email_content}\n Website: {website_content} \n Password: {password_content} \n Is it ok to save? ")
+
+        if is_ok:
+            with open("password.txt", mode= "a") as file:
+                file.writelines(website_content + "|" + email_content + "|" + password_content + "\n")
     
 def clear_content():
     website_entry.delete(0, END)
@@ -39,7 +67,7 @@ password_label.grid(column=0, row= 3)
 
 #Buttons
 
-generate_password_button = Button(text="Generate Password", width=10)
+generate_password_button = Button(text="Generate Password", width=10, command=generate_password)
 generate_password_button.grid(column=2, row= 3)
 
 add_button = Button(text="Add",width=36,command=lambda:[save_info(), clear_content()])
@@ -56,8 +84,6 @@ email_entry.insert(0, "khai@gmail.com")
 
 password_entry = Entry(width=21)
 password_entry.grid(column=1, row = 3, columnspan=1)
-
-
 
 window.mainloop()
 
